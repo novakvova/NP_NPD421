3b2e3616c294a3325ed6b7380b36f3ea30d2e5c1
novakvova <novakvova@gmail.com>
2025-06-08 14:17:54 +0300

    Init project

---
+++ SocketServer/SocketServer/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Привіт вітаю із св'ятом!");
